f9c24932812212446424de95eb18233e1a78c297
Brun02K23 <bvirinni@gmail.com>
2024-05-05 01:10:36 -0300

    documentando codigo + solucion de desafio de filtros sin middlewares

--- backend/src/routers/usuarios.routes.js
+++ backend/src/routers/usuarios.routes.js
@@ -2,6 +2,15 @@ import express from "express"
 import { usersServices } from "../services/usuarios.service.js"
 
 const router = express.Router() // en este router defino ENDPOINTS
+// La sintaxis es siempre la misma
+// router.metodo(path, async (req, res) => {
+//     try {
+
+//     } catch (error) {
+
+//     }
+// })
+
 router.get("/obtenerTodos", async (req, res) => {
     try {
         const usuarios = await usersServices.getAll()
@@ -33,6 +42,14 @@ router.post("/crearUsuario", async (req, res) => {
 })
 
 // TAREA: HACER UN GET BY FILTER DE USUARIOS (EL FILTRO LO DECIDEN UDS) (SOLO BACKEND)
+router.get("/byFilters", async (req, res) => {
+    try {
+        const response = await usersServices.getByFilters(req.query.nombre, req.query.apellido)
+        return res.json(response)
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+})
 
 // QUERY PARAM
 router.delete("/usuarioABorrar", async (req, res) => {
@@ -54,6 +71,8 @@ router.put("/modificar", async (req, res) => {
         return res.status(500).json({ error: error.message })
     }
 })
+
+
 
 const usuariosRouter = {
     router
--- backend/src/services/usuarios.service.js
+++ backend/src/services/usuarios.service.js
@@ -1,16 +1,19 @@
+import { Op } from "sequelize";
 import sequelize from "../databases/databases.js";
 
 // obtener todos los usuarios
 const getAll = async () => {
-    const allUsuarios = await sequelize.models.Usuarios.findAll()
-    return allUsuarios.map(usuario => usuario.dataValues)
+    const allUsuarios = await sequelize.models.Usuarios.findAll() // busco en sequelize todos los usuarios
+    return allUsuarios.map(usuario => usuario.dataValues) // por cada usuario, devuelvo unicamente sus atributos
 }
 
+// obtener por clave primaria
 const getById = async (id) => {
-    const usuario = await sequelize.models.Usuarios.findByPk(id)
-    return usuario.dataValues
+    const usuario = await sequelize.models.Usuarios.findByPk(id) // busco por clave primaria
+    return usuario.dataValues // retorno solo los atributos del usuario
 }
 
+// crear un nuevo usuario
 const createUser = async (body) => {
     const usuarioACrear = await sequelize.models.Usuarios.create({
         nombre: body.nombre,
@@ -22,22 +25,27 @@ const createUser = async (body) => {
     return usuarioACrear.dataValues
 }
 
+// actualizar usuario
+// el id sirve para buscarlo, el body para dar los nuevos valores de los atributos del usuario
 const updateUser = async (idUsuario, body) => {
-    const userToUpdate = await sequelize.models.Usuarios.findByPk(idUsuario)
+    const userToUpdate = await sequelize.models.Usuarios.findByPk(idUsuario) // busco por clave primaria
     if (!userToUpdate) { // si no existe tiro error
         throw new Error("Error, no existe ese usuario")
     }
 
+    // si existe seteo sus datos como lo que me dio el body
     userToUpdate.nombre = body.nombre
     userToUpdate.apellido = body.apellido
     userToUpdate.usuario = body.usuario
     userToUpdate.password = body.password
     userToUpdate.email = body.email
 
+    // espero a que guarde en base de datos
     await userToUpdate.save()
     return userToUpdate.dataValues
 }
 
+// borrar un usuario
 const deleteUser = async (idUsuario) => {
     const userToDelete = await sequelize.models.Usuarios.findByPk(idUsuario) // busco usuario
     if (!userToDelete) { // si no existe tiro error
@@ -47,12 +55,45 @@ const deleteUser = async (idUsuario) => {
     await userToDelete.destroy() // si existe lo borro
 }
 
+// obtener un usuario en funcion de una serie de filtros
+const getByFilters = async (nombre, apellido) => {
+    if (!nombre && !apellido) { // si no viene ningun filtro en el parametro, que retorne todos los usuarios
+        return await getAll()
+    }
+    // declaro un objeto de condiciones donde voy a ir almacenando los filtros
+    const whereConditions = {};
+
+    // si viene el nombre en el parametro de la peticion, que me cree una condicion para buscar el nombre
+    if (nombre) {
+        whereConditions.nombre = { [Op.like]: `%${nombre}%` };
+    }
+
+    // si viene el apellido en el parametro de la peticion, que me cree una condicion para buscar el apellido
+    if (apellido) {
+        whereConditions.apellido = { [Op.like]: `%${apellido}%` };
+    }
+
+    // busco los usuarios en funcion de los filtros
+    const usersFiltrados = await sequelize.models.Usuarios.findAll({
+        where: whereConditions
+    })
+
+    // si no hay usuarios que cumplan con la condicion, que retorne un error
+    if (usersFiltrados.length === 0) {
+        throw new Error("No hay usuarios que cumplan con los filtros")
+    }
+
+    // si hay usuarios que complan con los filtros, que por cada uno de ellos retorne solamente sus atributos
+    return usersFiltrados.map(user => user.dataValues)
+}
+
 const usersServices = {
     getAll,
     getById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    getByFilters
 }
 
 export { usersServices }
